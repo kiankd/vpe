c6098f82679e981cceae4153bc9ff97c4c3930fa
kiankd <kian.kenyon-dean@mail.mcgill.ca>
2015-11-10 16:22:54 -0500

    update step complete

--- optimize_mira_dual.py
+++ optimize_mira_dual.py
@@ -2,22 +2,16 @@ from gurobipy import *
 import numpy as np
 import math
 
-old = [1.0,1.0]
-best = [[0.5, 0.4], [0.3, 0.2]]
-gold = [1.0, 1.0]
-L = lambda x,y: np.linalg.norm(np.array(x)-np.array(y) , ord=1)
-C = 1.0
-
 def norm(x):
 	return (quicksum(xi*xi for xi in x))
 
 def slow_multiply(a, constants):
+	c = []
 	for i in range(len(constants)):
-		constants[i] = a * constants[i]
-	return quicksum(constants)
+		c.append(a * constants[i])
+	return quicksum(c)
 
-# def update_weights(old, best, gold, L, C=1.0):
-if __name__ == '__main__':
+def update_weights(old, best, gold, L, C=1.0):
 	primal_constraints = len(best)
 
 	m = Model("qp")
@@ -37,6 +31,7 @@ if __name__ == '__main__':
 	s = []
 	for k in range(primal_constraints):
 		s.append(slow_multiply(alpha[k], diffs[k]))
+
 	obj = -0.5 * norm(s) + quicksum(alpha[k] * L(gold, best[k]) for k in range(primal_constraints))\
 						 - quicksum(alpha[k] * np.dot(diffs[k], old) for k in range(primal_constraints))
 
@@ -46,7 +41,31 @@ if __name__ == '__main__':
 	m.optimize()
 
 	# Weight update step:
-	# sum(alpha[k].x * diffs[k] for k in range(primal_constraints))
+	return old + np.sum(alpha[k].x * np.array(diffs[k]) for k in range(primal_constraints))
+
+if __name__ == '__main__':
+	old = np.array([1.0,1.0])
+	best = [[0.5, 0.4], [0.3, 0.2]]
+	gold = [1.0, 1.0]
+	L = lambda x,y: np.linalg.norm(np.array(x)+np.array(y) , ord=1)
+	C = 1.0
+
+	w = [old, update_weights(old, best, gold, L, C=C)]
+	i = 1
+	while np.linalg.norm(w[i] - w[i-1]) >= 0.001:
+		w.append(update_weights(w[i], best, gold, L, C=C))
+		i += 1
+
+
+
+
+
+
+
+
+
+
+
 
 
 
