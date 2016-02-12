aecbce8eb3e986b2c5b266674ba0b0d3bb140cbf
Kian Kenyon-Dean <kiankd@gmail.com>
2016-02-12 11:37:48 -0500

    many more tests

--- gridsearch.py
+++ gridsearch.py
@@ -2,6 +2,7 @@ import itertools
 import numpy as np
 import word_characteristics as wc
 import random
+import os
 from detect_antecedents import AntecedentClassifier
 
 # Purpose:
@@ -14,26 +15,38 @@ def build_grid(params):
 
     return grid
 
+def completed_combos():
+    combos = set([])
+    for fname in os.listdir('tests/post_changes/'):
+        splitt = fname.split('_')
+        c, lr, k = float(splitt[1][1:]), float(splitt[2][2:]), float(splitt[3][1:])
+        combos.add((c,lr,k))
+    return combos
+
 if __name__ == '__main__':
     grid = build_grid(
          [[10.0**i for i in range(-4,2)],
           [10.0**i for i in range(-4,0)] + [0.99],
-          [5]]
+          [10,15,20]]
     )
-    random.shuffle(grid)
+    completed = completed_combos() 
+
+    print 'How much we need versus how much we did: %d, %d'%(len(grid),len(completed))
 
     a = AntecedentClassifier(0,14, 15,19, 20,24)
     a.initialize(['VP', wc.is_adjective, wc.is_verb], seed=9001, save=False, load=True, update=False)
 
     for combo in grid:
-        print "Current Params: ",combo
-        a.C = combo[0]
-        a.learn_rate = lambda x: combo[1]
-        name = 'full_c%s_lr%s_k%s_'%(combo[0], combo[1], combo[2])
+        if not combo in completed:
+            print "Current Params: ",combo
+            a.C = combo[0]
+            a.learn_rate = lambda x: combo[1]
+            name = 'full_c%s_lr%s_k%s_'%(combo[0], combo[1], combo[2])
 
-        a.fit(epochs=100, k=combo[2])
-        a.make_graphs(name)
-        np.save('saved_weights/'+name, np.array(a.W_avg))
+            a.fit(epochs=100, k=combo[2])
+            a.make_graphs(name)
+            np.save('saved_weights/'+name, np.array(a.W_avg))
 
-        a.reset()
-        a.initialize_weights(seed=9001)
+            a.reset()
+            a.initialize_weights(seed=9001)
+
